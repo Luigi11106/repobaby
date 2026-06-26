897570652caa66e75e3bfe502d9ad38d63ea5d58
Luis Baumgartner <luis.baumgartner@gmx.de>
2026-06-26 20:01:51 +0200

    imported a few datasets

--- code/01_download.R
+++ code/01_download.R
@@ -4,7 +4,10 @@
 
 library(here)
 
-# url <- "https://..."
-# download.file(url, destfile = here("data", "raw", "data.csv"))
+url <- "https://www.bundesfinanzministerium.de/Datenportal/Daten/offene-daten/wirtschaft-und-finanzen/s33-bruttoinlandsprodukt/datensaetze/csv_s33-bruttoinlandsprodukt.csv?__blob=publicationFile&v=10"
+download.file(url, destfile = here("data", "raw", "bip_data.csv"))
+
+cutunemp <- read_csv2(here("data", "raw", "unemployment_data_cut.csv"))
+# This file has been modified to become a clean dataset after the import.
 
 message("Raw data already present — delete data/raw/ and re-run to refresh.")
--- code/ZA8835.R
+++
@@ -1,6 +0,0 @@
-# This script is meant to shorten the size of the allbus dataset, so we are able to push it on GitHub.
-library(tidyverse)
-library(haven)
-big_allbus_data <- haven::read_dta("data/raw/ZA8835_v1-0-0.dta /ZA8835_v1-0-0.dta")
-small_allbus <- big_allbus_data %>% select(1:pv11, sex:mborn, eastwest:last_col())
-write_dta(small_allbus, "data/processed/allbus_mini.dta")
