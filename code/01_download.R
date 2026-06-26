22c96ff95013bf4e399da643182b3e9997912540
Luis Baumgartner <luis.baumgartner@gmx.de>
2026-06-26 22:18:56 +0200

    bip file download fixed

--- code/01_download.R
+++ code/01_download.R
@@ -4,10 +4,15 @@
 
 library(here)
 
-url <- "https://www.bundesfinanzministerium.de/Datenportal/Daten/offene-daten/wirtschaft-und-finanzen/s33-bruttoinlandsprodukt/datensaetze/csv_s33-bruttoinlandsprodukt.csv?__blob=publicationFile&v=10"
-download.file(url, destfile = here("data", "raw", "bip_data.csv"))
+bip <- read_csv2(
+  file = here("data", "raw", "bip.csv"),
+  skip = 3,
+  col_names = c("Jahr", "BIP_preisbereinigt", "Wachstum_preisbereinigt", "BIP_nominal", "Wachstum_nominal"),
+  locale = locale(encoding = "ISO-8859-1")
+)
+# This file was cut to become a clean data set after the import.
 
 cutunemp <- read_csv2(here("data", "raw", "unemployment_data_cut.csv"))
-# This file has been modified to become a clean dataset after the import.
+# This file was cut to become a clean data set after the import.
 
 message("Raw data already present — delete data/raw/ and re-run to refresh.")
