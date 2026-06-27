e257f25832347f8e5b5021f89a7c28bbcc3ef396
Luis Baumgartner <luis.baumgartner@gmx.de>
2026-06-27 13:24:59 +0200

    unemp import properly marked

--- code/01_download.R
+++ code/01_download.R
@@ -8,14 +8,19 @@ bip <- read_csv2(
   file = here("data", "raw", "bip.csv"),
   skip = 3,
   # The first 3 rows were filled with explanations.
-  col_names = c("jahr", "bip_preisbereinigt", "wachstum_preisbereinigt", "bip_nominal", "wachstum_nominal"),
+  col_names = c("year", "gdp_real", "growth_real", "gdp_nominal", "growth_nominal"),
   # The column names were not imported correctly and had to be added manually.
-  locale = locale(encoding = "ISO-8859-1"),
   n_max = 51
   # After row 51, the data set contained only NAs.
 )
 
-cutunemp <- read_csv2(here("data", "raw", "unemployment_data_cut.csv"))
-# This file was cut in the same way, to have the form of a clean data set
+unemp <- read_csv2(
+  file = here("data", "raw", "unemp.csv"),
+  skip = 8,
+  # Again, we skip the first few rows without values to fit the data correctly.
+  col_names = c("month", "unemployment_total", "inflow_unemployment", "outflow_unemployment", "unemployment_rate")
+  # The column names were proactively translated into English.
+)
+
 
 message("Raw data already present — delete data/raw/ and re-run to refresh.")
